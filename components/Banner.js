b4003892a34ab933efd8f4851ee304f6b59b60fd
Nelson Okunlola <nelly@nellys-tool.cable.rcn.com>
2020-09-21 12:47:31 -0500

    Fetch data

--- components/Banner.js
+++ components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, } from 'react-native';
 
 const Banner = ({title}) => (
-  <Text >{title}</Text>
+  <Text >{title  || '[loading...]'}</Text>
 );
 
 export default Banner;
